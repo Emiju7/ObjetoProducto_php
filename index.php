d82689cd6ba67fe337b0c9c6a40da4c48464c840
Emiju7 <emijugigante7@gmail.com>
2020-10-25 22:00:49 +0100

    Se crea el archivo de estilos para el index.

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index_style.css">
     <title>Creacion del objeto producto</title>
 </head>
 
 <body>
     <h2>CREACION DE UN NUEVO PRODUCTO:</h2>
 
-    <form id="producto_form" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+    <form id="product_form" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
         <label for="name">Nombre: </label><input id="name" type="text" placeholder="Introduczca el nombre de su producto" require><br>
         <label for="price">Precio: </label><input id="price" type="number" value="1" min="1" require><br>
-        <label for="description">Descripcion: </label><textarea id="description" form="product_form"></textarea><br>
         <label for="stock_number">Stock: </label> <input id="stock_number" type="number" value="1" min="1" require><br>
-
+        <label id="description_label" for="description">Descripcion: </label><textarea id="description" form="product_form"></textarea><br>
+        <input id="new_product" type="submit" value="Crear nuevo producto">
     </form>
 </body>
 
